fb1bb9e62b78d5adfa25442b8ee9cfbfc4651a7d
Noguchi Norimasa <norimasanoguchi@gmail.com>
2018-11-11 02:40:59 +0000

    投稿の削除機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,9 @@ class PostsController < ApplicationController
   end
   
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice:"削除しました"
   end
   
   private
